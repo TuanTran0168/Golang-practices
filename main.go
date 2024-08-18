8316f26340f2022b132a22f9bd777fee98cc1ff7
Trần Đăng Tuấn <82080825+TuanTran0168@users.noreply.github.com>
2024-08-18 21:50:34 +0700

    [ADD] Data Types

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import "fmt"
 
 func main() {
 	fmt.Print("\n")
-	var conferenceName = "Tuan Tran conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+
+	// var conferenceName string = "Tuan Tran Conference"
+	conferenceName := "Tuan Tran Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50 // not negative
+	// Data type
+	fmt.Printf("ConferenceTickets is (%T) and remainingTickets is (%T) and conferenceName is (%T)\n\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application!", conferenceName)
 	fmt.Print("Get your tickets here to attend!\n")
 
-	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
-	fmt.Print("Get your tickets here to attend!\n")
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
 
 	fmt.Print("\n")
 }
