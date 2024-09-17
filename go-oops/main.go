7d345f24ecf0c4112ba1a43e941812b9f2d55888
Trần Đăng Tuấn <tamthy12345@gmail.com>
2024-09-17 09:30:54 +0700

    [UPDATE][Golang-OOPs][Encapsulation] add Constructor

--- go-oops/main.go
+++ go-oops/main.go
@@ -8,9 +8,16 @@ import (
 func main() {
 	person := structs.Person{}
 
-	person.SetFirstName("Tuấn")
+	err := person.SetFirstName("Tuấn")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 	person.SetLastName("Trần")
 	person.SetAge(22)
 
-	fmt.Println(person)
+	person_1 := structs.NewPerson("Tuấn", "Trần", 22)
+
+	fmt.Println(person, person_1)
 }
